d66db07acf1d5c7c3cdabb5f2496bda64b1a559f
pallavkul <pallav.kul@gmail.com>
2016-12-14 12:45:58 +0530

    changes for removing spin comp

--- index.js
+++ index.js
@@ -6,37 +6,8 @@ var MergeTrees = require('broccoli-merge-trees');
 var Funnel = require('broccoli-funnel');
 
 module.exports = {
-  name: 'ember-spin-button2',
-
-  treeForVendor: function(tree) {
-
-    // Since spin.js is an npm module. we won't be able to use it directly.
-    // Modify the broccoli pipeline to include spin.js module in vendor.js
-
-    var nodes = ['spin.js'].map((function(pkgName){
-
-      var packagePath = path.dirname(require.resolve(pkgName));
-      var packageTree = new Funnel(this.treeGenerator(packagePath), {
-        srcDir: '/',
-        // destDir is the name of directory that ember-cli will create inside vendor directory
-        // this name is used in app.import(...) call in `included` hook
-        destDir: pkgName
-      });
-
-      return packageTree;
-
-    }).bind(this));
-
-
-    if (tree) {
-      nodes.unshift(tree);
-    }
-
-    return new MergeTrees(nodes);
+  name: 'ember-ambari-ui-components'
   },
-
-
-
 
   included: function(appOrAddon) {
     var app = appOrAddon.app || appOrAddon;
@@ -49,15 +20,6 @@ module.exports = {
     this._super.included && this._super.included.apply(this, arguments);
 
     app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-
-
-
-    app.import('vendor/spin.js/spin.js');
-    app.import('vendor/spinner.js', {
-      exports: {
-        spinner: ['default'],
-      },
-    });
 
   }
 
